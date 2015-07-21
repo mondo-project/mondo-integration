7a9fb48ba6654840a0dbed74c73683331578ccb3
Antonio García-Domínguez <antonio.garcia-dominguez@york.ac.uk>
2015-07-21 11:44:10 +0100

    Hawk: first version of an EMF driver for a remote model

--- uk.ac.york.mondo.integration.hawk.emf/src/uk/ac/york/mondo/integration/hawk/emf/HawkResourceImpl.java
+++ uk.ac.york.mondo.integration.hawk.emf/src/uk/ac/york/mondo/integration/hawk/emf/HawkResourceImpl.java
@@ -1,7 +1,18 @@
+/*******************************************************************************
+ * Copyright (c) 2015 University of York.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    Antonio Garcia-Dominguez - initial API and implementation
+ *******************************************************************************/
 package uk.ac.york.mondo.integration.hawk.emf;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.OutputStream;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
@@ -30,9 +41,33 @@ import org.slf4j.LoggerFactory;
 import uk.ac.york.mondo.integration.api.AttributeSlot;
 import uk.ac.york.mondo.integration.api.Hawk;
 import uk.ac.york.mondo.integration.api.ModelElement;
+import uk.ac.york.mondo.integration.api.ReferenceSlot;
 import uk.ac.york.mondo.integration.api.ScalarList._Fields;
 
+/**
+ * <p>
+ * EMF driver that reads a remote model from a Hawk index. The file format is a
+ * simple Java properties file, with the following keys:
+ * </p>
+ * <ul>
+ * <li>{@link #PROPERTY_HAWK_URL} (required) is the URL to the remote Hawk index
+ * (e.g. <code>http://127.0.0.1:8080/thrift/hawk</code>.</li>
+ * <li>{@link #PROPERTY_HAWK_INSTANCE} (required) is the name of the Hawk
+ * instance within the server.
+ * <li>{@link #PROPERTY_HAWK_REPOSITORY} (optional) is the URL of the VCS
+ * repository within Hawk that contains the models of interest, or
+ * <code>*</code> if all repositories should be considered (the default).
+ * <li>{@link #PROPERTY_HAWK_FILES} (optional) is a comma-separated list of file
+ * patterns to filter (such as <code>*.xmi</code>), or <code>*</code> if all
+ * files should be considered (the default).
+ * </ul>
+ */
 public class HawkResourceImpl extends ResourceImpl {
+
+	public static final String PROPERTY_HAWK_FILES = "hawk.files";
+	public static final String PROPERTY_HAWK_REPOSITORY = "hawk.repository";
+	public static final String PROPERTY_HAWK_INSTANCE = "hawk.instance";
+	public static final String PROPERTY_HAWK_URL = "hawk.url";
 
 	private static final Logger LOGGER = LoggerFactory.getLogger(HawkResourceImpl.class);
 
@@ -41,9 +76,8 @@ public class HawkResourceImpl extends ResourceImpl {
 	private String hawkRepository;
 	private String[] hawkFilePatterns;
 
-	private Map<String, EObject> nodeIdToEObjectMap;
-
-	public HawkResourceImpl() {}
+	public HawkResourceImpl() {
+	}
 
 	public HawkResourceImpl(URI uri) {
 		super(uri);
@@ -86,107 +120,258 @@ public class HawkResourceImpl extends ResourceImpl {
 		final Properties props = new Properties();
 		props.load(inputStream);
 
-		this.hawkURL = requiredProperty(props, "hawk.url");
-		this.hawkInstance = requiredProperty(props, "hawk.instance");
-		this.hawkRepository = optionalProperty(props, "hawk.repository", "*");
-		this.hawkFilePatterns = optionalProperty(props, "hawk.files", "*").split(",");
+		this.hawkURL = requiredProperty(props, PROPERTY_HAWK_URL);
+		this.hawkInstance = requiredProperty(props, PROPERTY_HAWK_INSTANCE);
+		this.hawkRepository = optionalProperty(props, PROPERTY_HAWK_REPOSITORY, "*");
+		this.hawkFilePatterns = optionalProperty(props, PROPERTY_HAWK_FILES, "*").split(",");
 		try {
 			final Hawk.Client client = new Hawk.Client(new TCompactProtocol(new THttpClient(hawkURL)));
-			final List<ModelElement> elems = client.getModel(
-				hawkInstance, hawkRepository, Arrays.asList(hawkFilePatterns)
-			);
+			final List<ModelElement> elems = client.getModel(hawkInstance,
+					hawkRepository, Arrays.asList(hawkFilePatterns));
 
-			// Do a first pass, creating all the objects with their attributes and saving their graph IDs
+			// Do a first pass, creating all the objects with their attributes
+			// and saving their graph IDs
 			// into the One Map to bind them.
 			final Registry packageRegistry = getResourceSet().getPackageRegistry();
-			this.nodeIdToEObjectMap = new HashMap<>();
+			final Map<String, EObject> nodeIdToEObjectMap = new HashMap<>();
 			for (ModelElement me : elems) {
-				final EPackage pkg = packageRegistry.getEPackage(me.metamodelUri);
-				final EClassifier eClassifier = pkg.getEClassifier(me.typeName);
-				if (!(eClassifier instanceof EClass)) {
-					throw new IOException(String.format("Received an element of type '%s', which is not an EClass", eClassifier));
-				}
-				final EClass eClass = (EClass)eClassifier;
-
-				final EFactory factory = packageRegistry.getEFactory(me.metamodelUri);
+				final EClass eClass = getEClass(packageRegistry, me);
+				final EFactory factory = packageRegistry
+						.getEFactory(me.metamodelUri);
 				final EObject obj = factory.create(eClass);
 				nodeIdToEObjectMap.put(me.id, obj);
+				getContents().add(obj);
 
 				for (AttributeSlot s : me.attributes) {
-					if (!s.isSet) continue;
+					if (!s.isSet)
+						continue;
 					setStructuralFeatureFromSlot(eClass, obj, s);
 				}
 			}
 
-			// TODO On the second pass, fill in the references
+			// On the second pass, fill in the references
+			for (ModelElement me : elems) {
+				final EObject sourceObj = nodeIdToEObjectMap.get(me.id);
+
+				for (ReferenceSlot s : me.references) {
+					if (!s.isSet)
+						continue;
+
+					final EClass eClass = getEClass(packageRegistry, me);
+					final EStructuralFeature feature = eClass.getEStructuralFeature(s.name);
+					if (feature.isMany()) {
+						final EList<EObject> value = new BasicEList<>();
+						for (String targetId : s.ids) {
+							final EObject targets = nodeIdToEObjectMap
+									.get(targetId);
+							if (targets == null) {
+								LOGGER.warn(
+										"Could not find ModelElement with id {} for feature {} of class {}, skipping",
+										targetId, feature, eClass);
+								continue;
+							}
+							value.add(targets);
+						}
+
+						sourceObj.eSet(feature, value);
+					} else {
+						final String targetId = s.ids.get(0);
+						final EObject target = nodeIdToEObjectMap.get(targetId);
+						if (target == null) {
+							LOGGER.warn(
+									"Could not find ModelElement with id {} for feature {} of class {}, skipping",
+									targetId, feature, eClass);
+							continue;
+						}
+						sourceObj.eSet(feature, target);
+					}
+				}
+			}
 		} catch (TException e) {
+			LOGGER.error(e.getMessage(), e);
 			throw new IOException(e);
+		} catch (IOException e) {
+			LOGGER.error(e.getMessage(), e);
+			throw e;
 		}
 	}
 
+	
+
+	@Override
+	protected void doSave(OutputStream outputStream, Map<?, ?> options) throws IOException {
+		throw new UnsupportedOperationException();
+	}
+
+	private EClass getEClass(final Registry packageRegistry, ModelElement me)
+			throws IOException {
+		final EPackage pkg = packageRegistry.getEPackage(me.metamodelUri);
+		if (pkg == null) {
+			throw new IOException(String.format("Could not find EPackage with URI '%s' in the registry %s", me.metamodelUri, packageRegistry));
+		}
+
+		final EClassifier eClassifier = pkg.getEClassifier(me.typeName);
+		if (!(eClassifier instanceof EClass)) {
+			throw new IOException(String.format(
+					"Received an element of type '%s', which is not an EClass",
+					eClassifier));
+		}
+		final EClass eClass = (EClass) eClassifier;
+		return eClass;
+	}
+
 	private EStructuralFeature setStructuralFeatureFromSlot(
-		final EClass eClass, final EObject eObject, AttributeSlot slot
-	) throws IOException {
+			final EClass eClass, final EObject eObject, AttributeSlot slot)
+			throws IOException {
 		assert slot.isSet : "This method should only be called for slots with set values";
 
-		final EStructuralFeature feature = eClass.getEStructuralFeature(slot.name);
+		final EStructuralFeature feature = eClass
+				.getEStructuralFeature(slot.name);
 		final EClassifier eType = feature.getEType();
 
 		if (feature.isMany()) {
 			switch (eType.getClassifierID()) {
 			case EcorePackage.EBYTE:
-				if (!slot.values.isSetVBytes()) {
-					throw new IOException(String.format(
-						"Expected to receive bytes for feature '%s' in type '%s', but did not",
-						feature.getName(), eClass.getName()));
-				} else {
-					final EList<Byte> bytes = new BasicEList<Byte>(slot.values.getVBytes().length);
-					for (byte b : slot.values.getVBytes()) {
-						bytes.add(b);
-					}
-					eObject.eSet(feature, bytes);
-				}
+				// TODO not sure, need to test
+				setByteEListStructuralFeature(eClass, eObject, slot, feature);
 				break;
 
 			case EcorePackage.EFLOAT:
 				if (!slot.values.isSetVDoubles()) {
-					throw new IOException(String.format(
-							"Expected to receive doubles for feature '%s' in type '%s', but did not",
-							feature.getName(), eClass.getName()));
-					
+					throw new IOException(
+							String.format(
+									"Expected to receive doubles for feature '%s' in type '%s', but did not",
+									feature.getName(), eClass.getName()));
+
 				} else {
-					final EList<Float> floats = new BasicEList<Float>(slot.values.getVDoubles().size());
+					final EList<Float> floats = new BasicEList<Float>(
+							slot.values.getVDoubles().size());
 					for (double d : slot.values.getVDoubles()) {
-						floats.add((float)d);
+						floats.add((float) d);
 					}
 					eObject.eSet(feature, floats);
 				}
 				break;
 
 			case EcorePackage.EDOUBLE:
-				setManyStructuralFeatureWithExpectedType(eClass, eObject, slot, feature, _Fields.V_DOUBLES); break;
+				setManyStructuralFeatureWithExpectedType(eClass, eObject, slot,
+						feature, _Fields.V_DOUBLES);
+				break;
 			case EcorePackage.EINT:
-				setManyStructuralFeatureWithExpectedType(eClass, eObject, slot, feature, _Fields.V_INTEGERS); break;
+				setManyStructuralFeatureWithExpectedType(eClass, eObject, slot,
+						feature, _Fields.V_INTEGERS);
+				break;
 			case EcorePackage.ELONG:
-				setManyStructuralFeatureWithExpectedType(eClass, eObject, slot, feature, _Fields.V_LONGS); break;
+				setManyStructuralFeatureWithExpectedType(eClass, eObject, slot,
+						feature, _Fields.V_LONGS);
+				break;
 			case EcorePackage.ESHORT:
-				setManyStructuralFeatureWithExpectedType(eClass, eObject, slot, feature, _Fields.V_SHORTS); break;
+				setManyStructuralFeatureWithExpectedType(eClass, eObject, slot,
+						feature, _Fields.V_SHORTS);
+				break;
 			case EcorePackage.ESTRING:
-				setManyStructuralFeatureWithExpectedType(eClass, eObject, slot, feature, _Fields.V_STRINGS); break;
+				setManyStructuralFeatureWithExpectedType(eClass, eObject, slot,
+						feature, _Fields.V_STRINGS);
+				break;
 			default:
-				throw new IOException(String.format("Unknown data type %s with isMany = true", eType.getName()));
+				throw new IOException(String.format(
+						"Unknown data type %s with isMany = true",
+						eType.getName()));
 			}
 		} else {
+			// isSet=true and many=false means that we should have exactly one
+			// value
 			switch (eType.getClassifierID()) {
 			case EcorePackage.EBYTE_ARRAY:
-				// TODO same as isMany + BYTE?
+				// TODO not sure, need to test
+				setByteEListStructuralFeature(eClass, eObject, slot, feature);
 				break;
-			// TODO rest of the types
+			case EcorePackage.EBYTE:
+				if (!slot.values.isSetVBytes()) {
+					throw new IOException(
+							String.format(
+									"Expected to receive bytes for feature '%s' in type '%s', but did not",
+									feature.getName(), eClass.getName()));
+				} else {
+					final byte b = slot.values.getVBytes()[0];
+					eObject.eSet(feature, b);
+				}
+				break;
+			case EcorePackage.EFLOAT:
+				if (!slot.values.isSetVDoubles()) {
+					throw new IOException(
+							String.format(
+									"Expected to receive doubles for feature '%s' in type '%s', but did not",
+									feature.getName(), eClass.getName()));
+
+				} else {
+					final double d = slot.values.getVDoubles().get(0);
+					eObject.eSet(feature, (float) d);
+				}
+				break;
+			case EcorePackage.EDOUBLE:
+				setSingleStructuralFeatureWithExpectedType(eClass, eObject,
+						slot, feature, _Fields.V_DOUBLES);
+				break;
+			case EcorePackage.EINT:
+				setSingleStructuralFeatureWithExpectedType(eClass, eObject,
+						slot, feature, _Fields.V_INTEGERS);
+				break;
+			case EcorePackage.ELONG:
+				setSingleStructuralFeatureWithExpectedType(eClass, eObject,
+						slot, feature, _Fields.V_LONGS);
+				break;
+			case EcorePackage.ESHORT:
+				setSingleStructuralFeatureWithExpectedType(eClass, eObject,
+						slot, feature, _Fields.V_SHORTS);
+				break;
+			case EcorePackage.ESTRING:
+				setSingleStructuralFeatureWithExpectedType(eClass, eObject,
+						slot, feature, _Fields.V_STRINGS);
+				break;
 			default:
-				throw new IOException(String.format("Unknown data type %s with isMany = false", eType.getName()));
+				throw new IOException(String.format(
+						"Unknown data type %s with isMany = false",
+						eType.getName(), feature.isMany()));
 			}
 		}
 		return feature;
+	}
+
+	private void setSingleStructuralFeatureWithExpectedType(
+			final EClass eClass, final EObject eObject, AttributeSlot slot,
+			final EStructuralFeature feature, final _Fields expectedType)
+			throws IOException {
+		if (!slot.values.isSet(expectedType)) {
+			throw new IOException(
+					String.format(
+							"Expected to receive '%s' for feature '%s' in type '%s' with many='%s', but did not",
+							expectedType, feature.getName(), eClass.getName(),
+							feature.isMany()));
+		} else {
+			final Iterable<?> slotValues = (Iterable<?>) slot.values
+					.getFieldValue(expectedType);
+			final Object first = slotValues.iterator().next();
+			eObject.eSet(feature, first);
+		}
+	}
+
+	private void setByteEListStructuralFeature(final EClass eClass,
+			final EObject eObject, AttributeSlot slot,
+			final EStructuralFeature feature) throws IOException {
+		if (!slot.values.isSetVBytes()) {
+			throw new IOException(
+					String.format(
+							"Expected to receive bytes for feature '%s' in type '%s', but did not",
+							feature.getName(), eClass.getName()));
+		} else {
+			final EList<Byte> bytes = new BasicEList<Byte>(
+					slot.values.getVBytes().length);
+			for (byte b : slot.values.getVBytes()) {
+				bytes.add(b);
+			}
+			eObject.eSet(feature, bytes);
+		}
 	}
 
 	private void setManyStructuralFeatureWithExpectedType(final EClass eClass,
@@ -194,15 +379,20 @@ public class HawkResourceImpl extends ResourceImpl {
 			final EStructuralFeature feature, final _Fields expectedType)
 			throws IOException {
 		if (!slot.values.isSet(expectedType)) {
-			throw new IOException(String.format(
-					"Expected to receive '%s' for feature '%s' in type '%s', but did not",
-					expectedType, feature.getName(), eClass.getName()));
+			throw new IOException(
+					String.format(
+							"Expected to receive '%s' for feature '%s' in type '%s' with many=%s, but did not",
+							expectedType, feature.getName(), eClass.getName(),
+							feature.isMany()));
 		} else {
-			eObject.eSet(feature, ECollections.toEList((Iterable<?>) slot.values.getFieldValue(expectedType)));
+			eObject.eSet(feature, ECollections
+					.toEList((Iterable<?>) slot.values
+							.getFieldValue(expectedType)));
 		}
 	}
 
-	private String requiredProperty(Properties props, String name) throws IOException {
+	private String requiredProperty(Properties props, String name)
+			throws IOException {
 		final String value = (String) props.get(name);
 		if (value == null) {
 			throw new IOException(name + " has not been set");
@@ -210,10 +400,12 @@ public class HawkResourceImpl extends ResourceImpl {
 		return value;
 	}
 
-	private String optionalProperty(Properties props, String name, String defaultValue) throws IOException {
+	private String optionalProperty(Properties props, String name,
+			String defaultValue) throws IOException {
 		final String value = (String) props.get(name);
 		if (value == null) {
-			LOGGER.info("{} has not been set, using {} as default", name, defaultValue);
+			LOGGER.info("{} has not been set, using {} as default", name,
+					defaultValue);
 			return defaultValue;
 		}
 		return value;
