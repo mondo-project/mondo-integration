5d186ff99a0b13e7c05c3433a97ef7f3f32780f7
Antonio García-Domínguez <nyoescape@gmail.com>
2015-09-16 13:01:22 +0100

    api: generate JavaScript code for Thrift as well

--- uk.ac.york.mondo.integration.api/generate.sh
+++ uk.ac.york.mondo.integration.api/generate.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
-rm -rf src-gen
-mkdir src-gen
+rm -rf src-gen src-gen-js
+mkdir src-gen src-gen-js
 thrift -out src-gen --gen java src/api.thrift
+thrift -out src-gen-js --gen js src/api.thrift
