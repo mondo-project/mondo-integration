6eff20250fdd866413d012de7e3ee920798425ec
Antonio García-Domínguez <nyoescape@gmail.com>
2015-11-19 16:36:46 +0000

    Generate client update site through Tycho as well

--- uk.ac.york.mondo.integration.updatesite/to-github.sh
+++ uk.ac.york.mondo.integration.updatesite/to-github.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 
-./to-maven.sh
+#./to-maven.sh
 
-DEST_P2="$HOME/Documents/mondo-updates/mondo-updates"
+DEST_P2="$HOME/Documents/mondo-integration-ghpages/client-updates"
 rm -rf "$DEST_P2"
 mkdir "$DEST_P2"
 
-for i in plugins features content.jar artifacts.jar category.xml; do
+pushd target/repository
+for i in plugins features content.jar artifacts.jar ../../category.xml; do
   cp -r "$i" "$DEST_P2"
 done
+popd
 
 cd "$DEST_P2"
 git add --all
