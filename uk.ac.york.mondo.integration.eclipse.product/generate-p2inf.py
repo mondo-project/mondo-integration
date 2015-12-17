ed8b7b195e25266584fa1aaa8c499939a67cbaa7
Antonio García-Domínguez <nyoescape@gmail.com>
2015-12-17 22:20:38 +0000

    Update target platform and build new products with Modelio EXML driver, server-specific CLI and IFC export

--- uk.ac.york.mondo.integration.eclipse.product/generate-p2inf.py
+++ uk.ac.york.mondo.integration.eclipse.product/generate-p2inf.py
@@ -45,6 +45,7 @@ ENTRIES_NOGPL = (
     ('org.hawk.emfresource.feature.feature.group', '[1.0.0,2.0.0)'),
     ('org.hawk.epsilon.feature.feature.group', '[1.0.0,2.0.0)'),
     ('org.hawk.localfolder.feature.feature.group', '[1.0.0,2.0.0)'),
+    ('org.hawk.modelio.exml.feature.feature.group', '[1.0.0,2.0.0)'),
     ('org.hawk.orientdb.feature.feature.group', '[1.0.0,2.0.0)'),
     ('org.hawk.osgiserver.feature.feature.group', '[1.0.0,2.0.0)'),
     ('org.hawk.svn.feature.feature.group', '[1.0.0,2.0.0)'),
@@ -72,12 +73,12 @@ ENTRIES_NOGPL = (
     ('org.mondo.wt.cstudy.editor.feature.feature.group', '[1.0.0,2.0.0)'),
     ('org.mondo.wt.cstudy.merge.feature.feature.group', '[1.0.0,2.0.0)'),
     ('org.mondo.wt.cstudy.model.feature.feature.group', '[1.0.0,2.0.0)'),
-    # ('es.ikerlan.wt.demo.feature.feature.group', '[1.0.0,2.0.0)'),
 )
 
 ENTRIES_GPL = (
     ('org.hawk.neo4jv2.feature.feature.group', '[1.0.0,2.0.0)'),
     ('org.hawk.ifc.feature.feature.group', '[1.0.0,2.0.0)'),
+    ('uk.ac.york.mondo.integration.hawk.emf.ifcexport.feature.feature.group', '[1.0.0,2.0.0)'),
     ('org.hawk.modelio.feature.feature.group', '[1.0.0,2.0.0)'),
 )
 
