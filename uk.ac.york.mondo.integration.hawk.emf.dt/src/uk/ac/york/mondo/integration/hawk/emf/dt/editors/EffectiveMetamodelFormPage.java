fab6c0f0ad82e2be0f871e9c2019ed968309b0b5
Antonio García-Domínguez <nyoescape@gmail.com>
2016-01-28 23:25:18 +0000

    hawk.emf.dt: improve performance of effective metamodel tree editor for large metamodels (e.g. IFC2x3/IFC4)

--- uk.ac.york.mondo.integration.hawk.emf.dt/src/uk/ac/york/mondo/integration/hawk/emf/dt/editors/EffectiveMetamodelFormPage.java
+++ uk.ac.york.mondo.integration.hawk.emf.dt/src/uk/ac/york/mondo/integration/hawk/emf/dt/editors/EffectiveMetamodelFormPage.java
@@ -17,7 +17,6 @@ import java.util.TreeMap;
 
 import org.apache.thrift.TException;
 import org.eclipse.jface.viewers.CheckStateChangedEvent;
-import org.eclipse.jface.viewers.CheckboxTreeViewer;
 import org.eclipse.jface.viewers.ICheckStateListener;
 import org.eclipse.jface.viewers.ICheckStateProvider;
 import org.eclipse.jface.viewers.ITableLabelProvider;
@@ -25,10 +24,13 @@ import org.eclipse.jface.viewers.ITreeContentProvider;
 import org.eclipse.jface.viewers.LabelProvider;
 import org.eclipse.jface.viewers.Viewer;
 import org.eclipse.swt.SWT;
+import org.eclipse.swt.events.SelectionAdapter;
+import org.eclipse.swt.events.SelectionEvent;
 import org.eclipse.swt.graphics.Image;
 import org.eclipse.swt.layout.FillLayout;
 import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.TreeItem;
 import org.eclipse.ui.dialogs.ContainerCheckedTreeViewer;
 import org.eclipse.ui.forms.IManagedForm;
 import org.eclipse.ui.forms.editor.FormPage;
@@ -52,6 +54,81 @@ import uk.ac.york.mondo.integration.hawk.emf.dt.Activator;
 import uk.ac.york.mondo.integration.hawk.emf.impl.HawkResourceImpl;
 
 public class EffectiveMetamodelFormPage extends FormPage {
+
+	private final class MetamodelTreeViewer extends ContainerCheckedTreeViewer {
+		private MetamodelTreeViewer(Composite parent) {
+			super(parent);
+		}
+
+		public TreeItem findTreeItem(Object o) {
+			return (TreeItem)this.findItem(o);
+		}
+	}
+
+	private final class MetamodelCheckStateListener implements ICheckStateListener {
+		@Override
+		public void checkStateChanged(CheckStateChangedEvent event) {
+			if (event.getElement() instanceof MetamodelNode) {
+				final MetamodelNode mn = (MetamodelNode)event.getElement();
+				if (event.getChecked()) {
+					for (Node child : mn.children) {
+						store.addType(mn.label, child.label);
+					}
+				} else {
+					for (Node child : mn.children) {
+						store.removeType(mn.label, child.label);
+					}
+				}
+			}
+			else if (event.getElement() instanceof TypeNode) {
+				final TypeNode tn = (TypeNode)event.getElement();
+				if (event.getChecked()) {
+					store.addType(tn.parent.label, tn.label);
+				} else {
+					store.removeType(tn.parent.label, tn.label);
+				}
+			} else if (event.getElement() instanceof SlotNode) {
+				final SlotNode sn = (SlotNode)event.getElement();
+				final TypeNode tn = (TypeNode)sn.parent;
+				final MetamodelNode mn = (MetamodelNode)tn.parent;
+				ImmutableSet<String> slots = store.getIncludedSlots(mn.label, tn.label);
+				ImmutableSet<String> newSlots;
+				if (slots == null) {
+					slots = ImmutableSet.of();
+				}
+
+				if (event.getChecked()) {
+					newSlots = new ImmutableSet.Builder<String>().addAll(slots).add(sn.label).build();
+				} else {
+					final Builder<String> builder = new ImmutableSet.Builder<>();
+					for (Node n : tn.children) {
+						if (treeViewer.getChecked(n)) {
+							builder.add(n.label);
+						}
+					}
+					newSlots = builder.build();
+				}
+
+				if (newSlots.size() != tn.children.size()) {
+					store.addType(mn.label, tn.label, newSlots);
+				} else {
+					store.addType(mn.label, tn.label);
+				}
+			}
+
+			// We *don't* want to check anything that's not visible - too expensive for large metamodels!
+			final TreeItem treeItem = treeViewer.findTreeItem(event.getElement());
+			checkItemRecursively(treeItem, event.getChecked());
+			getEditor().setDirty(true);
+		}
+
+		private void checkItemRecursively(TreeItem treeItem, boolean newState) {
+			treeItem.setChecked(newState);
+			for (TreeItem child : treeItem.getItems()) {
+				checkItemRecursively(child, newState);
+			}
+		}
+	}
 
 	public class MetamodelCheckStateProvider implements ICheckStateProvider {
 
@@ -109,7 +186,8 @@ public class EffectiveMetamodelFormPage extends FormPage {
 	}
 
 	private EffectiveMetamodelStore store = new EffectiveMetamodelStore();
-	private CheckboxTreeViewer treeViewer;
+	private MetamodelTreeViewer treeViewer;
+	private MetamodelCheckStateProvider checkStateProvider;
 
 	private static final class MetamodelLabelProvider extends LabelProvider implements ITableLabelProvider {
 		@Override
@@ -159,7 +237,7 @@ public class EffectiveMetamodelFormPage extends FormPage {
 	}
 
 	private final class MetamodelContentProvider implements ITreeContentProvider {
-		private Object[] roots = null;
+		protected Object[] roots = null;
 
 		@Override
 		public void inputChanged(Viewer viewer, Object oldInput, Object newInput) {
@@ -276,65 +354,14 @@ public class EffectiveMetamodelFormPage extends FormPage {
 		cTable.setLayoutData(new TableWrapData(TableWrapData.FILL_GRAB));
 		cTable.setLayout(Utils.createTableWrapLayout(2));
 
-		treeViewer = new ContainerCheckedTreeViewer(cTable);
-		treeViewer.setContentProvider(new MetamodelContentProvider());
+		treeViewer = new MetamodelTreeViewer(cTable);
+		final MetamodelContentProvider contentProvider = new MetamodelContentProvider();
+		treeViewer.setUseHashlookup(true);
+		treeViewer.setContentProvider(contentProvider);
 		treeViewer.setLabelProvider(new MetamodelLabelProvider());
-		treeViewer.setCheckStateProvider(new MetamodelCheckStateProvider());
-		treeViewer.addCheckStateListener(new ICheckStateListener() {
-			@Override
-			public void checkStateChanged(CheckStateChangedEvent event) {
-				if (event.getElement() instanceof MetamodelNode) {
-					final MetamodelNode mn = (MetamodelNode)event.getElement();
-					if (event.getChecked()) {
-						for (Node child : mn.children) {
-							store.addType(mn.label, child.label);
-						}
-					} else {
-						for (Node child : mn.children) {
-							store.removeType(mn.label, child.label);
-						}
-					}
-				}
-				else if (event.getElement() instanceof TypeNode) {
-					final TypeNode tn = (TypeNode)event.getElement();
-					if (event.getChecked()) {
-						store.addType(tn.parent.label, tn.label);
-					} else {
-						store.removeType(tn.parent.label, tn.label);
-					}
-				} else if (event.getElement() instanceof SlotNode) {
-					final SlotNode sn = (SlotNode)event.getElement();
-					final TypeNode tn = (TypeNode)sn.parent;
-					final MetamodelNode mn = (MetamodelNode)tn.parent;
-					ImmutableSet<String> slots = store.getIncludedSlots(mn.label, tn.label);
-					ImmutableSet<String> newSlots;
-					if (slots == null) {
-						slots = ImmutableSet.of();
-					}
-
-					if (event.getChecked()) {
-						newSlots = new ImmutableSet.Builder<String>().addAll(slots).add(sn.label).build();
-					} else {
-						final Builder<String> builder = new ImmutableSet.Builder<>();
-						for (Node n : tn.children) {
-							if (treeViewer.getChecked(n)) {
-								builder.add(n.label);
-							}
-						}
-						newSlots = builder.build();
-					}
-
-					if (newSlots.size() != tn.children.size()) {
-						store.addType(mn.label, tn.label, newSlots);
-					} else {
-						store.addType(mn.label, tn.label);
-					}
-				}
-
-				treeViewer.setSubtreeChecked(event.getElement(), event.getChecked());
-				getEditor().setDirty(true);
-			}
-		});
+		checkStateProvider = new MetamodelCheckStateProvider();
+		treeViewer.setCheckStateProvider(checkStateProvider);
+		treeViewer.addCheckStateListener(new MetamodelCheckStateListener());
 		treeViewer.setInput(store);
 		treeViewer.getTree().setLayoutData(new TableWrapData(TableWrapData.FILL_GRAB));
 		final Composite cButtons = toolkit.createComposite(cTable, SWT.WRAP);
@@ -345,8 +372,30 @@ public class EffectiveMetamodelFormPage extends FormPage {
 
 		final Button btnSelectAll = new Button(cButtons, SWT.NONE);
 		btnSelectAll.setText("Select all");
+		btnSelectAll.addSelectionListener(new SelectionAdapter() {
+			@SuppressWarnings("deprecation")
+			@Override
+			public void widgetSelected(SelectionEvent e) {
+				for (Object on : contentProvider.roots) {
+					final MetamodelNode mn = (MetamodelNode)on;
+					for (Node tn : mn.children) {
+						store.addType(mn.label, tn.label);
+					}
+				}
+				treeViewer.setAllChecked(true);
+				getEditor().setDirty(true);
+			}
+		});
+
 		final Button btnDeselectAll = new Button(cButtons, SWT.NONE);
 		btnDeselectAll.setText("Deselect all");
+		btnDeselectAll.addSelectionListener(new SelectionAdapter() {
+			@Override
+			public void widgetSelected(SelectionEvent e) {
+				setEffectiveMetamodelStore(new EffectiveMetamodelStore());
+			}
+		});
+
 		// TODO add UI for Analyze... button
 	}
 
@@ -359,5 +408,6 @@ public class EffectiveMetamodelFormPage extends FormPage {
 		if (treeViewer != null) {
 			treeViewer.setInput(this.store);
 		}
+		getEditor().setDirty(true);
 	}
 }
