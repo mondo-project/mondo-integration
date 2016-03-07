1f7701fe574c6f2b349292b53915fc2d60db4b72
Antonio García-Domínguez <nyoescape@gmail.com>
2016-03-07 12:22:48 +0000

    First draft of an integration between EMF-Splitter and Hawk

---
+++ uk.ac.york.mondo.integration.hawk.emfsplitter/src/uk/ac/york/mondo/integration/hawk/emfsplitter/HawkCrossReferences.java
@@ -0,0 +1,127 @@
+/*******************************************************************************
+ * Copyright (c) 2016 University of York.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    Antonio Garcia-Dominguez - initial API and implementation
+ *******************************************************************************/
+package uk.ac.york.mondo.integration.hawk.emfsplitter;
+
+import java.io.File;
+import java.util.Arrays;
+
+import org.eclipse.emf.common.util.BasicEList;
+import org.eclipse.emf.common.util.EList;
+import org.eclipse.emf.common.util.URI;
+import org.eclipse.emf.ecore.EClass;
+import org.eclipse.emf.ecore.resource.Resource;
+import org.hawk.core.IStateListener.HawkState;
+import org.hawk.core.IVcsManager;
+import org.hawk.core.runtime.LocalHawkFactory;
+import org.hawk.emfresource.impl.LocalHawkResourceImpl;
+import org.hawk.orientdb.OrientDatabase;
+import org.hawk.osgiserver.HManager;
+import org.hawk.osgiserver.HModel;
+import org.hawk.workspace.Workspace;
+import org.mondo.modular.references.ext.IEditorCrossReferences;
+
+/**
+ * Integrates Hawk into the cross reference selector dialog for EMF-Splitter. At
+ * the moment, it indexes the whole workspace into a local Hawk instance, which
+ * is directly accessed through the underlying graph.
+ */
+public class HawkCrossReferences implements IEditorCrossReferences {
+
+	private static final String HAWK_INSTANCE = "emfsplitter";
+
+	@Override
+	public boolean init(String modularNature) {
+		try {
+			// Ensure the Hawk instance is ready
+			getHawkInstance();
+		} catch (Exception e) {
+			HawkCrossReferencesPlugin.getDefault().logError(e);
+			return false;
+		}
+		return true;
+	}
+
+	@Override
+	public boolean finish(String modularNature) {
+		// Nothing to do - Hawk shuts down by itself using a workbench listener
+		return true;
+	}
+
+	@Override
+	public boolean isGlobal() {
+		// The Hawk Workspace VCS doesn't allow filtering projects yet
+		return true;
+	}
+
+	@Override
+	public boolean isNature(String modularNature) {
+		// The Hawk Workspace VCS doesn't allow filtering projects yet
+		return true;
+	}
+
+	@Override
+	public EList<?> getChoicesOfValues(String modularNature, Resource res, EClass anEClass) {
+		LocalHawkResourceImpl hawkResource = null;
+		try {
+			// Ensure that Hawk is available and running
+			HModel hawkInstance = getHawkInstance();
+			if (!hawkInstance.isRunning()) {
+				hawkInstance.start(HManager.getInstance());
+			}
+			hawkInstance.getHawk().getModelIndexer().waitFor(HawkState.RUNNING);
+
+			// Look for the Hawk resource in the resource set first
+			for (Resource r : res.getResourceSet().getResources()) {
+				if (r instanceof LocalHawkResourceImpl) {
+					hawkResource = (LocalHawkResourceImpl)r;
+					if (hawkResource.getIndexer().getName().equals(HAWK_INSTANCE)) {
+						// This is the EMF-Splitter local Hawk resource
+						break;
+					}
+				}
+			}
+
+			// If it's not in the resource set, load it and add it to the resource set
+			if (hawkResource == null) {
+				hawkResource = new LocalHawkResourceImpl(URI.createURI("hawk://"), hawkInstance.getIndexer(), true, Arrays.asList("*"), Arrays.asList("*"));
+				hawkResource.load(null);
+				res.getResourceSet().getResources().add(hawkResource);
+			}
+
+			// Fetch all the instances of the provided class
+			return hawkResource.fetchNodes(anEClass, true);
+		} catch (Exception e) {
+			HawkCrossReferencesPlugin.getDefault().logError(e);
+			return new BasicEList<Object>();
+		}
+	}
+
+	protected HModel getHawkInstance() throws Exception {
+		final HManager hawkManager = HManager.getInstance();
+		synchronized (hawkManager) {
+			HModel hawkInstance = hawkManager.getHawkByName(HAWK_INSTANCE);
+			if (hawkInstance == null) {
+				final File storageFolder = new File("hawk");
+
+				// TODO: limit plugins to EMF, use Neo4j if available
+				hawkInstance = HModel.create(new LocalHawkFactory(), HAWK_INSTANCE, storageFolder,
+							storageFolder.toURI().toASCIIString(), OrientDatabase.class.getName(), null, hawkManager,
+							hawkManager.getCredentialsStore(), 0, 0);
+
+				final IVcsManager repo = new Workspace();
+				hawkInstance.addVCS(repo.getLocation(), repo.getClass().getName(), "", "", false);
+			}
+	
+			return hawkInstance;
+		}
+	}
+
+}
---
+++ uk.ac.york.mondo.integration.hawk.emfsplitter/src/uk/ac/york/mondo/integration/hawk/emfsplitter/HawkCrossReferencesPlugin.java
@@ -0,0 +1,44 @@
+/*******************************************************************************
+ * Copyright (c) 2016 University of York.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    Antonio Garcia-Dominguez - initial API and implementation
+ *******************************************************************************/
+package uk.ac.york.mondo.integration.hawk.emfsplitter;
+
+import org.eclipse.core.runtime.IStatus;
+import org.eclipse.core.runtime.Plugin;
+import org.eclipse.core.runtime.Status;
+import org.osgi.framework.BundleContext;
+
+/**
+ * Plugin activator.
+ */
+public class HawkCrossReferencesPlugin extends Plugin {
+
+	private static HawkCrossReferencesPlugin INSTANCE;
+
+	@Override
+	public void start(BundleContext context) throws Exception {
+		super.start(context);
+		INSTANCE = this;
+	}
+
+	@Override
+	public void stop(BundleContext context) throws Exception {
+		super.stop(context);
+		INSTANCE = null;
+	}
+
+	public static HawkCrossReferencesPlugin getDefault() {
+		return INSTANCE;
+	}
+
+	public void logError(Throwable t) {
+		getLog().log(new Status(IStatus.ERROR, getBundle().getSymbolicName(), t.getMessage(), t));
+	}
+}
