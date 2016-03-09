9e46f713be500e5178a8c62b1dbef1a9c4908cdb
Antonio García-Domínguez <nyoescape@gmail.com>
2016-03-09 10:57:02 +0000

    emfsplitter: checks for res == null in #getChoicesOfValues

--- uk.ac.york.mondo.integration.hawk.emfsplitter/src/uk/ac/york/mondo/integration/hawk/emfsplitter/HawkCrossReferences.java
+++ uk.ac.york.mondo.integration.hawk.emfsplitter/src/uk/ac/york/mondo/integration/hawk/emfsplitter/HawkCrossReferences.java
@@ -27,6 +27,8 @@ import org.eclipse.emf.ecore.EClass;
 import org.eclipse.emf.ecore.EObject;
 import org.eclipse.emf.ecore.EPackage;
 import org.eclipse.emf.ecore.resource.Resource;
+import org.eclipse.emf.ecore.resource.ResourceSet;
+import org.eclipse.emf.ecore.resource.impl.ResourceSetImpl;
 import org.hawk.core.IMetaModelResourceFactory;
 import org.hawk.core.IStateListener.HawkState;
 import org.hawk.core.IVcsManager;
@@ -113,12 +115,14 @@ public class HawkCrossReferences implements IEditorCrossReferences {
 			final HModel hawkInstance = getHawkInstance();
 
 			// Look for the Hawk resource in the resource set first
-			for (Resource r : res.getResourceSet().getResources()) {
-				if (r instanceof LocalHawkResourceImpl) {
-					hawkResource = (LocalHawkResourceImpl)r;
-					if (hawkResource.getIndexer().getName().equals(HAWK_INSTANCE)) {
-						// This is the EMF-Splitter local Hawk resource
-						break;
+			if (res != null) {
+				for (Resource r : res.getResourceSet().getResources()) {
+					if (r instanceof LocalHawkResourceImpl) {
+						hawkResource = (LocalHawkResourceImpl) r;
+						if (hawkResource.getIndexer().getName().equals(HAWK_INSTANCE)) {
+							// This is the EMF-Splitter local Hawk resource
+							break;
+						}
 					}
 				}
 			}
@@ -127,7 +131,9 @@ public class HawkCrossReferences implements IEditorCrossReferences {
 			if (hawkResource == null) {
 				hawkResource = new LocalHawkResourceImpl(URI.createURI("hawk://"), hawkInstance.getIndexer(), true, Arrays.asList("*"), Arrays.asList("*"));
 				hawkResource.load(null);
-				res.getResourceSet().getResources().add(hawkResource);
+				if (res != null) {
+					res.getResourceSet().getResources().add(hawkResource);
+				}
 			}
 
 			if (modularNature != null) {
